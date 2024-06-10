6a646988e57c6184082b91a46f178a54a4184faa
choieuihyun <amammmaa1@naver.com>
2024-06-10 16:10:31 +0900

    ViewPager 글 작성

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
-    img: "img/user/profile-licat.png",
+    username: "의현",
+    company: "home",
+    position: "home_ceo",
+    img: "img/user/rammus.jpeg",
   },
 ];
 
